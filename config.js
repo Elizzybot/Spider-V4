660f134b58799752028b94516e0944da5b7e6634
Elizzybot <elijahchinecheremonah@gmail.com>
2024-08-19 11:07:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,9 +23,9 @@ global.location = '1' //Ganti Pake Location Panel Lu
 //===========================//
 
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['254104301695']
-global.ownMain = '254104301695'
-global.NamaOwner = '𝕶𝖎𝖓𝖌 𝕾𝖆𝖒' //gausah diganti 
+global.owner = ['2347038494997']
+global.ownMain = '2347038494997'
+global.NamaOwner = 'no-name tech' //gausah diganti 
 global.sessionName = 'spidersession'
 global.connect = true // Ubah Ke False Jika Ingin Menggunakan Qr Code
 global.namabot = '𝐒𝐏𝐈𝐃𝐄𝐑-𝐕4' //ganti aj klo mau
